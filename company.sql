6b11e68d738f95224f96a30bcafe65e44fdbe565
Santiago Donoso <santiagodonoso@gmail.com>
2024-02-07 11:48:06 +0100

    .

--- company.sql
+++ company.sql
@@ -4,23 +4,31 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     user_pk         TEXT,
     user_name       TEXT,
+    user_price      TEXT,
     PRIMARY KEY(user_pk)
 ) WITHOUT ROWID;
 
 -- Insert data into the users table
-INSERT INTO users (user_pk, user_name) VALUES ("842d9ed8-1e4c-438a-8980-b67f0d5e9994", "One");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e620f", "Two");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e630f", "Three");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e640f", "Four");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e655f", "Five");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e660f", "Six");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e670f", "Seven");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e680f", "Eight");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e690f", "Nine");
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e611f", "Ten");
+INSERT INTO users (user_pk, user_name, user_price) VALUES ("842d9ed8-1e4c-438a-8980-b67f0d5e9994", "One", 10);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f2113e620f", "Two", 20);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f3113e630f", "Three", 30);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f4113e640f", "Four", 40);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f5113e655f", "Five", 50);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e660f", "Six", 60);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e670f", "Seven", 70);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e680f", "Eight", 80);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e690f", "Nine", 90);
+INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e611f", "Ten", 100);
 
 -- Show all users
 SELECT * FROM users;
+
+SELECT * FROM users ORDER BY user_name ASC;
+SELECT * FROM users ORDER BY user_name DESC;
+
+SELECT * FROM users ORDER BY user_price ASC;
+
+
 
 -- Delete user by id
 DELETE FROM users WHERE user_pk = "842d9ed8-1e4c-438a-8980-b67f0d5e9994";
