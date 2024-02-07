36f3d50fa3fbda57e98838245e01ed3860e0ab66
Santiago Donoso <santiagodonoso@gmail.com>
2024-02-07 12:18:57 +0100

    .

--- company.sql
+++ company.sql
@@ -4,21 +4,21 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     user_pk         TEXT,
     user_name       TEXT,
-    user_price      TEXT,
+    user_price      INTEGER,
     PRIMARY KEY(user_pk)
 ) WITHOUT ROWID;
 
 -- Insert data into the users table
-INSERT INTO users (user_pk, user_name, user_price) VALUES ("842d9ed8-1e4c-438a-8980-b67f0d5e9994", "One", 10);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f2113e620f", "Two", 20);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f3113e630f", "Three", 30);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f4113e640f", "Four", 40);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f5113e655f", "Five", 50);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e660f", "Six", 60);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e670f", "Seven", 70);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e680f", "Eight", 80);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e690f", "Nine", 90);
-INSERT INTO users VALUES ("f6912761-b085-4e9c-ad26-74f6113e611f", "Ten", 100);
+INSERT INTO users (user_pk, user_name, user_price) VALUES ("1", "One", 10);
+INSERT INTO users VALUES ("2", "Two", 20.20);
+INSERT INTO users VALUES ("3", "Three", 30);
+INSERT INTO users VALUES ("4", "Four", 40);
+INSERT INTO users VALUES ("5", "Five", 50);
+INSERT INTO users VALUES ("6", "Six", 60);
+INSERT INTO users VALUES ("7", "Seven", 70);
+INSERT INTO users VALUES ("8", "Eight", 80);
+INSERT INTO users VALUES ("9", "Nine", 90);
+INSERT INTO users VALUES ("10", "Ten", 100);
 
 -- Show all users
 SELECT * FROM users;
@@ -29,14 +29,31 @@ SELECT * FROM users ORDER BY user_name DESC;
 SELECT * FROM users ORDER BY user_price ASC;
 
 
+--                                            start  get
+SELECT * FROM users ORDER BY user_price ASC LIMIT 0, 2;
+
+-- LIKE wildcards
+-- Name that start with an specific letter
+SELECT * FROM users WHERE user_name LIKE "a%";
+SELECT * FROM users WHERE user_name LIKE "mi%";
+SELECT * FROM users WHERE user_name LIKE "%sen";
+SELECT * FROM users WHERE user_name LIKE "%en";
+SELECT * FROM users WHERE user_name LIKE "%oa%";  -- soap
+
+-- Create a SQL command that gets any user containing the "i" in the name
+SELECT * FROM users WHERE user_name LIKE "%i%";
+
+-- Create a SQL command that gets any user containing the "f" at the end
+SELECT * FROM users WHERE user_name LIKE "%f";
+
+-- SQL you can use AND, you can also use OR
+-- Create a SQL command that gets any user name 
+-- containing the "i" in the name AND the price is above 1
+-- above >
+SELECT * FROM users WHERE user_name LIKE "%i%" AND user_price > 60;
 
 -- Delete user by id
 DELETE FROM users WHERE user_pk = "842d9ed8-1e4c-438a-8980-b67f0d5e9994";
-
-
-
-
-
 
 
 -- Create a table to test how to build a compound/composite key
