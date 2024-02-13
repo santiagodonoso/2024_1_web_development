9d1e4f91070490cb9aa935785ea966587a7bf14e
Santiago Donoso <santiagodonoso@gmail.com>
2024-02-13 11:11:41 +0100

    .

--- routes/create_user.py
+++ routes/create_user.py
@@ -7,7 +7,9 @@ def _():
     try:
         # VALIDATION
         user_name = request.forms.get("user_name")
-        return f"Hi {user_name}"
+        user_last_name = request.forms.get("user_last_name")
+        # Show Hi A Aa
+        return f"Hi {user_name} {user_last_name}"
     except Exception as ex:
         print(ex)
     finally:
