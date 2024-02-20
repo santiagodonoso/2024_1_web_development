f13b6181a4bfdea539364c18b0995c00ce89f726
Santiago Donoso <santiagodonoso@gmail.com>
2024-02-20 11:19:22 +0100

    .

--- routes/create_item.py
+++ routes/create_item.py
@@ -1,11 +1,18 @@
 from bottle import post, request
+import uuid
+import random
+import x
 
 ##############################
 @post("/items")
 def _():
     try:
         item_name = request.forms.get("item_name")
-
+        item_id = uuid.uuid4().hex
+        item_price = random.uniform(10.5, 999.99)
+        db = x.db()
+        q = db.execute("INSERT INTO items VALUES(?, ?, ?)", (item_id, item_name, item_price))
+        db.commit()
         return f"""
         <template mix-target="#items" mix-top>
             <div>
