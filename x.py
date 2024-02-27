e360fc428e203fd1b933bf58f0de028f6e52ba73
Santiago Donoso <santiagodonoso@gmail.com>
2024-02-27 10:00:29 +0100

    .

--- x.py
+++ x.py
@@ -47,8 +47,33 @@ def validate_user_last_name():
     return "ok"
 
 
+##############################
+USER_PASSWORD_MIN = 6
+USER_PASSWORD_MAX = 50
+
+def validate_user_password():
+	error = f"user_password {USER_PASSWORD_MIN} to {USER_PASSWORD_MAX} characters"
+	user_password = request.forms.get("user_password", "")
+	user_password = user_password.strip()
+	if len(user_password) < USER_PASSWORD_MIN : raise Exception(400, error)
+	if len(user_password) > USER_PASSWORD_MAX : raise Exception(400, error)
+	return user_password
 
 
+##############################
+
+USER_EMAIL_MIN = 6
+USER_EMAIL_MAX = 100
+USER_EMAIL_REGEX = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
+
+def validate_user_email():
+	error = f"user_email invalid"
+	user_email = request.forms.get("user_email", "")        
+	user_email = user_email.strip()
+	if len(user_email) < USER_EMAIL_MIN : raise Exception(400, error)
+	if len(user_email) > USER_EMAIL_MAX : raise Exception(400, error)  
+	if not re.match(USER_EMAIL_REGEX, user_email): raise Exception(400, error)
+	return user_email
 
 
 
