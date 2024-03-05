85a0b2c4b3d002b09fe136262dfeec23a36901a6
Santiago Donoso <santiagodonoso@gmail.com>
2024-03-05 09:04:03 +0100

    ...

--- api_html/signup.py
+++ api_html/signup.py
@@ -1,8 +1,25 @@
 from bottle import post, request
+import x
 
 @post("/signup")
 def _():
-    return """
-    <template>
-    </template>
-    """
+    try:
+        # Validate
+        x.validate_user_email()
+
+        return """
+        <template>
+        </template>
+        """
+    except Exception as ex:
+        print(ex)
+        if "user_email" in ex.args[1]:
+            return """
+            <template mix-target="#message">
+            <div id="message">
+                Email invalid
+            </div>
+            </template>    
+            """
+    finally:
+        pass
